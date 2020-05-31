fb16af39c63240d4069681918c30fe38e15d577a
Gilles Caulier <caulier.gilles@gmail.com>
2020-05-31 18:03:17 +0200

    rename dirs

--- src/bordertools/StandardBordersFactory.cpp
+++ src/bordertools/StandardBordersFactory.cpp
@@ -5,7 +5,6 @@
  *
  * Date        : 2011-09-01
  * Description : a plugin to create photo layouts by fusion of several images.
- * 
  *
  * Copyright (C) 2011      by Lukasz Spas <lukasz dot spas at gmail dot com>
  * Copyright (C) 2009-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
@@ -26,8 +25,6 @@
 #include "StandardBordersFactory.h"
 #include "PolaroidBorderDrawer.h"
 #include "SolidBorderDrawer.h"
-
-
 
 using namespace PhotoLayoutsEditor;
 
