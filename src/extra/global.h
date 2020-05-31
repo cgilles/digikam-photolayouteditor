6f6b9d9c4d3d14932b58fcc318c86467bbb1690b
Gilles Caulier <caulier.gilles@gmail.com>
2020-05-31 18:13:06 +0200

    polish

--- src/extra/global.h
+++ src/extra/global.h
@@ -5,10 +5,9 @@
  *
  * Date        : 2011-09-01
  * Description : a plugin to create photo layouts by fusion of several images.
- * 
  *
  * Copyright (C) 2011      by Lukasz Spas <lukasz dot spas at gmail dot com>
- * Copyright (C) 2009-2015 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2009-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
@@ -26,7 +25,7 @@
 #ifndef PLE_GLOBAL_H
 #define PLE_GLOBAL_H
 
-#define Q_DELETE(ptr)   if(ptr) { delete ptr; ptr = 0; }
+#define Q_DELETE(ptr)   if (ptr) { delete ptr; ptr = nullptr; }
 
 #include <QUndoCommand>
 #include <QPrinter>
